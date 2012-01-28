02f04802c249a99f61c4c9367e685e56fec3e249
Matt Snider <matt.snider@gmail.com>
2012-01-28 02:23:29 -0500

    database helper and sql.xml created with workout table listed.

--- Fido90x/src/com/fidotechnologies/fido90tracker/launch.java
+++ Fido90x/src/com/fidotechnologies/fido90tracker/launch.java
@@ -1,7 +1,11 @@
 package com.fidotechnologies.fido90tracker;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class launch extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,22 @@ public class launch extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    }
+        }
+    
+public void programSelect (View v)
+{
+	final CharSequence[] items = {"Classic","Cardio", "Doubles"};
+
+	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	builder.setTitle("Select Program");
+	builder.setItems(items, new DialogInterface.OnClickListener() {
+	    public void onClick(DialogInterface dialog, int item) {
+	       // Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+	       // Intent in = new Intent(jitapp.this, webview.class);
+	      //  in.putExtra(bar, items[item]);
+			//startActivity(in);
+	    }});
+	AlertDialog alert = builder.create();
+	alert.show();
+}
 }
